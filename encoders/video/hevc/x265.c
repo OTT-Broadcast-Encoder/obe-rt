d96983675833392e4bddda52d659d05a226349fb
Steven Toth <stoth@ltnglobal.com>
2020-04-15 20:37:13 -0400

    hevc: Fix the VBV buffer to a user supplied value, don't scale by 2x 4x or 24x etc

--- encoders/video/hevc/x265.c
+++ encoders/video/hevc/x265.c
@@ -819,9 +819,7 @@ static int reconfigure_encoder(struct context_s *ctx)
 
 	if (ctx->h->obe_system == OBE_SYSTEM_TYPE_LOWEST_LATENCY) {
 		/* Found that in lowest mode, obe doesn't accept the param, but the codec reports underruns. */
-		ctx->enc_params->avc_param.rc.i_vbv_buffer_size = ctx->enc_params->avc_param.rc.i_vbv_max_bitrate * 24;
-		ctx->enc_params->avc_param.rc.i_vbv_buffer_size = ctx->enc_params->avc_param.rc.i_vbv_max_bitrate * 4;
-		//ctx->enc_params->avc_param.rc.i_vbv_buffer_size = ctx->enc_params->avc_param.rc.i_vbv_max_bitrate;
+		ctx->enc_params->avc_param.rc.i_vbv_buffer_size = ctx->enc_params->avc_param.rc.i_vbv_max_bitrate;
 		printf(MESSAGE_PREFIX "vbv_bufsize = %d\n", ctx->enc_params->avc_param.rc.i_vbv_buffer_size);
 	}
 	sprintf(&val[0], "%d", ctx->enc_params->avc_param.rc.i_vbv_buffer_size);
