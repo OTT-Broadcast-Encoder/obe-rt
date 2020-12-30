d546362150bb2f348134ae38a1afafd60e953de1
Steven Toth <steven.toth@ltnglobal.com>
2020-12-30 17:19:35 -0500

    core v210 fonts: Added functions to draw ascii directly into a packet V210 frame

--- input/sdi/v210.h
+++ input/sdi/v210.h
@@ -9,6 +9,17 @@
 extern "C" {
 #endif
 
+struct V210_painter_s
+{
+        uint32_t *frame;
+        uint32_t *ptr;
+
+        int widthPixels;
+        int heightPixels;
+        int strideBytes;
+	int interlaced;
+};
+
 /* KL paint 6 pixels in a single point */
 __inline__ void V210_draw_6_pixels(uint32_t *addr, uint32_t *coloring);
 __inline__ void V210_draw_box(uint32_t *frame_addr, uint32_t stride, int color, int interlaced);
@@ -16,6 +27,10 @@ __inline__ void V210_draw_box_at(uint32_t *frame_addr, uint32_t stride, int colo
 void V210_write_32bit_value(void *frame_bytes, uint32_t stride, uint32_t value, uint32_t lineNr, int interlaced);
 
 uint32_t V210_read_32bit_value(void *frame_bytes, uint32_t stride, uint32_t lineNr, double scalefactor);
+
+/* Write text to V210 packed planes */
+int  V210_painter_reset(struct V210_painter_s *ctx, uint8_t *frame, int widthPixels, int heightPixels, int strideBytes, int interlaced);
+void V210_painter_draw_ascii_at(struct V210_painter_s *ctx, int x, int y, const char *str);
 
 #ifdef __cplusplus
 };
