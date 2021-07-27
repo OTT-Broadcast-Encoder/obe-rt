02d0c75d529794bb22743406260e2aa78360760b
Steven Toth <steven.toth@ltnglobal.com>
2021-07-27 09:38:43 -0400

    core: metadata add helper functions and accessors for metadata items

--- common/metadata.h
+++ common/metadata.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <pthread.h>
 
-enum avmetadata_item_type_e { AVMETADATA_UNDEFINED, AVMETADATA_SECTION_SCTE35 };
+enum avmetadata_item_type_e { AVMETADATA_UNDEFINED, AVMETADATA_SECTION_SCTE35, AVMETADATA_VANC_SCTE104 };
 struct avmetadata_item_s
 {
 	enum  avmetadata_item_type_e item_type;
@@ -15,6 +15,9 @@ struct avmetadata_item_s
 	/* SCTE35 */
 	/* We need this for the codec to be able to hand SCTE to the muxer, only the decklink device knows this. */
 	int outputStreamId;
+
+	/* VANC */
+	int lineNr;
 };
 
 struct avmetadata_s
@@ -28,6 +31,9 @@ void avmetadata_init(struct avmetadata_s *);  /* One time call during startup / 
 void avmetadata_reset(struct avmetadata_s *); /* Erase any prior items */
 void avmetadata_clone(struct avmetadata_s *dst, struct avmetadata_s *src);
 
+/* Count the number of metadata items in the array matching item_type 'type' */
+int  avmetadata_count_by_type(struct avmetadata_s *src, enum avmetadata_item_type_e item_type);
+
 struct avmetadata_item_s *avmetadata_item_alloc(int lengthBytes, enum avmetadata_item_type_e item_type);
 struct avmetadata_item_s *avmetadata_item_clone(struct avmetadata_item_s *src);
 void avmetadata_item_dprintf(int fd, struct avmetadata_item_s *src);
@@ -35,6 +41,10 @@ const char *avmetadata_item_name(struct avmetadata_item_s *src);
 
 int avmetadata_item_data_write(struct avmetadata_item_s *dst, const unsigned char *buf, int lengthBytes);
 int avmetadata_item_data_realloc(struct avmetadata_item_s *src, int lengthBytes);
+int avmetadata_item_data_set_linenr(struct avmetadata_item_s *src, int lineNr);
+int avmetadata_item_data_get_linenr(struct avmetadata_item_s *src);
+int avmetadata_item_data_set_outputstreamid(struct avmetadata_item_s *src, int outputstreamid);
+int avmetadata_item_data_get_outputstreamid(struct avmetadata_item_s *src);
 
 void avmetadata_item_free(struct avmetadata_item_s *);
 
