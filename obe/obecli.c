3564afde8e680ad6298f16086ce0ed02f137b20e
Steven Toth <steven.toth@ltnglobal.com>
2022-04-07 18:26:44 -0400

    Added debug command to inject multi-part fragmented SCTE104 message for testing

--- obe/obecli.c
+++ obe/obecli.c
@@ -1258,6 +1258,7 @@ extern int g_decklink_histogram_reset;
 extern int g_decklink_histogram_print_secs;
 extern int g_decklink_render_walltime;
 extern int g_decklink_inject_scte104_preroll6000;
+extern int g_decklink_inject_scte104_fragmented;
 
 /* Case 1 */
 extern int g_decklink_fake_lost_payload;
@@ -1373,6 +1374,8 @@ extern time_t g_decklink_missing_video_last_time;
         g_decklink_render_walltime == 0 ? "disabled" : "enabled");
     printf("sdi_input.inject_scte104_preroll6000 = %d\n",
         g_decklink_inject_scte104_preroll6000);
+    printf("sdi_input.inject_scte104_fragmented = %d\n",
+        g_decklink_inject_scte104_fragmented);
     printf("sdi_input.inject_frame_enable = %d [%s]\n",
         g_decklink_inject_frame_enable,
         g_decklink_inject_frame_enable == 0 ? "disabled" : "enabled");
@@ -1551,6 +1554,9 @@ static int set_variable(char *command, obecli_command_t *child)
     } else
     if (strcasecmp(var, "sdi_input.inject_scte104_preroll6000") == 0) {
         g_decklink_inject_scte104_preroll6000 = val;
+    } else
+    if (strcasecmp(var, "sdi_input.inject_scte104_fragmented") == 0) {
+        g_decklink_inject_scte104_fragmented = val;
     } else
     if (strcasecmp(var, "sdi_input.fake_every_other_frame_lose_audio_payload") == 0) {
         g_decklink_fake_every_other_frame_lose_audio_payload = val;
