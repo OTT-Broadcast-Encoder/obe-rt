1400a9cb2edd6e45965a0cd040f2f8964cc34b6d
Steven Toth <steven.toth@ltnglobal.com>
2023-07-17 13:34:23 -0400

    vega3311: refactoring audio code

---
+++ input/sdi/vega/vega-3311-audio.cpp
@@ -0,0 +1,46 @@
+#if HAVE_VEGA3311_CAP_TYPES_H
+
+/*****************************************************************************
+ * vega-misc.cpp: Audio/Video from the VEGA encoder cards
+ *****************************************************************************
+ * Copyright (C) 2021 LTN
+ *
+ * Authors: Steven Toth <stoth@ltnglobal.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *****************************************************************************/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <inttypes.h>
+#include <iostream>
+#include <limits.h>
+#include <fcntl.h>
+#include <semaphore.h>
+#include <time.h>
+
+#include "vega-3311.h"
+
+extern "C"
+{
+#include <libklvanc/vanc.h>
+#include <libklscte35/scte35.h>
+}
+
+#endif /* #if HAVE_VEGA3311_CAP_TYPES_H */
